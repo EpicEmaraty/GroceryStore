e02449abfadb343328d74f498cd6c07703af9391
epicemaraty <realnuaimi@mail.csuchico.edu>
2018-02-23 19:02:13 -0800

    DONE!

--- main.cpp
+++ main.cpp
@@ -8,13 +8,27 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-while (input!='q'&&input!='Q'){
+  string item;
+do {  
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-}
+  if (input == 'a' || input == 'A')
+{
+ cout<<"What is the item?"<<endl;
+ cin>>item;
+
+ if (numItems<5)
+ {
+ list[numItems] = item;
+ numItems++;
+} else { cout<<"You'll need a bigger list!"<<endl;}
+
+ }
+
+} while (input!='q'&&input!='Q');
   return 0;
 }
